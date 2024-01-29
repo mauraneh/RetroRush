6cf365b60056913e7595be32633a5f0645e9cc32
MHUG <maurane.hugon@gmail.com>
2024-01-29 16:49:29 +0100

    first screen ok, homepage en cours

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import logo from '../assets/images/logoRR.png';
 
 function HomePage() {
   const location = useLocation();
   const { nickname } = location.state || { nickname: 'Anonymous' };
 
   return (
-    <div>
-      <h1>Welcome, {nickname}</h1>
-      
+    <div className='Container-hp'>
+      <div className='Header-hp'>
+        <img src={logo} className="Logo" alt="logo" />
+        <h1 className='Title-hp'>Let's play, {nickname} !</h1>
+      </div>
     </div>
   );
 }
